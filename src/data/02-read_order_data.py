f49ad8798a1ba1df3e5c3d58f26059311e8e5e11
patrickp89 <patrickpreuss@netherspace.de>
2019-05-01 17:26:00 +0200

    refactor: made the column-join more generic

--- src/data/02-read_order_data.py
+++ src/data/02-read_order_data.py
@@ -1,24 +1,24 @@
-import pandas as pandas
-from typing import List
+import pandas as pd
 import os
+import os.path
 
-def read_order_data() -> pandas.DataFrame:
+def read_data_with_columns(data_file: str, column_names_file: str) -> pd.DataFrame:
+    """ Joins a CSV data file with its corresponding column names and returns the pandas.DataFrame """
+    if os.path.isfile(data_file):
+        if os.path.isfile(column_names_file):
+            with open(column_names_file) as cf:
+                column_names = list(map(lambda column : column.split(':')[0], cf))
+                return pd.read_csv(data_file, names = column_names)
+        else:
+            print('The columns file does not exist!')
+    else:
+        print('The data file does not exist!')
+
+
+def read_order_data() -> pd.DataFrame:
     """ Reads in the order data and returns it in a pandas.DataFrame """
-    
-    # get current directory and compute path to file
-    dirname = os.path.dirname(__file__)
-    orderColumnsFileName = os.path.join(dirname, r'../../data/raw/orders/order_columns.txt')
+    return read_data_with_columns(r'data/raw/orders/order_data.csv', r'data/raw/orders/order_columns.txt')
 
-    rawColumnNames: List[str] = [];
-    # read in line by line into list
-    with open(orderColumnsFileName, "r") as f:
-        rawColumnNames = list(f)
-
-    # split each column at the ':' and take the first part so we have the pure column name
-    columnNames = list(map(lambda column : column.split(':')[0], rawColumnNames))
-
-    # transform csv into data frame with the column names
-    orderDataFileName = os.path.join(dirname, r'../../data/raw/orders/order_data.csv')
-    df = pandas.read_csv(orderDataFileName, names = columnNames)
-    
-    return df
+def read_clickstream_data() -> pd.DataFrame:
+    """ Reads in the clickstream data and returns it in a pandas.DataFrame """
+    return read_data_with_columns(r'data/interim/clickstream/clickstream_data.csv', r'data/raw/clickstream/clickstream_columns.txt')
