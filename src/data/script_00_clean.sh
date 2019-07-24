196f5ee2e9f61f4031b170b0257beb8b448a0faf
patrickp89 <patrickpreuss@netherspace.de>
2019-07-24 13:05:47 +0200

    fix: unzipping now also workz under windows...

--- src/data/script_00_clean.sh
+++ src/data/script_00_clean.sh
@@ -7,6 +7,7 @@ rm -f notebooks/*.tex
 rm -f notebooks/*.pdf
 rm -f notebooks/*.html
 rm -f notebooks/*.log
+rm -f notebooks/*.csv
 rm -rf notebooks/product-stats_files/
 rm -f reports/figures/*
 rm -f Rplots.pdf
