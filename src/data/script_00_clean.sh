50366a74bb46faea87a0906d9f15a0ce8d884016
patrickp89 <patrickpreuss@netherspace.de>
2019-07-24 10:37:59 +0200

    added a zoomed in partial view of the decision tree

--- src/data/script_00_clean.sh
+++ src/data/script_00_clean.sh
@@ -6,6 +6,7 @@ rm -rf data/interim/joined/*
 rm -f notebooks/*.tex
 rm -f notebooks/*.pdf
 rm -f notebooks/*.html
+rm -f notebooks/*.log
 rm -rf notebooks/product-stats_files/
 rm -f reports/figures/*
-
+rm -f Rplots.pdf
